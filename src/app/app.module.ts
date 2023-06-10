b73d84411ec92b2c188169abe59458aeac8b88c2
jaime rey <jaimereycasado@gmail.com>
2023-06-11 01:20:24 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-
+//import { AppComponent } from './app.component';
+import { ModelModule } from './model/model.module';
+import { CoreModule } from './core/core.module';
+import { TableComponent } from './core/table.component';
+import { FormComponent } from './core/form.component';
+import { MessageModule } from './messages/message.module';
+import { MessageComponent } from './messages/message.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [],
+  imports: [BrowserModule, ModelModule, CoreModule, MessageModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [TableComponent, FormComponent, MessageComponent],
 })
-export class AppModule { }
+export class AppModule {}
