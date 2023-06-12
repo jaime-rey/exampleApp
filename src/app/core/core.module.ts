f93e2ffd5c4e585ea27d07247d57be3cf61c2446
jaime rey <jaimereycasado@gmail.com>
2023-06-13 00:36:48 +0200

    working form group

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ModelModule } from '../model/model.module';
 import { TableComponent } from './table.component';
 import { FormComponent } from './form.component';
 import { SharedState } from './sharedState.service';
+import { ValidationHelper } from './validation_ helper';
+
 @NgModule({
-  imports: [BrowserModule, FormsModule, ModelModule],
-  declarations: [TableComponent, FormComponent],
+  imports: [BrowserModule, FormsModule, ModelModule, ReactiveFormsModule],
+  declarations: [TableComponent, FormComponent, ValidationHelper],
   exports: [ModelModule, TableComponent, FormComponent],
   providers: [SharedState],
 })
--- src/app/core/form.component.ts
+++ src/app/core/form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, NgForm, Validators, FormGroup } from '@angular/forms';
 import { Product } from '../model/product.model';
 import { Model } from '../model/repository.model';
 import { Message } from '../messages/message.model';
@@ -13,6 +13,21 @@ import { MODES, SharedState, StateUpdate } from './sharedState.service';
 export class FormComponent {
   product: Product = new Product();
   editing: boolean = false;
+  productForm: FormGroup = new FormGroup({
+    name: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern('^[A-Za-z ]+$'),
+      ],
+      updateOn: 'change',
+    }),
+    category: new FormControl('', { validators: Validators.required }),
+    price: new FormControl('', {
+      validators: [Validators.required, Validators.pattern('^[0-9.]+$')],
+    }),
+  });
+
   constructor(
     private model: Model,
     private state: SharedState,
@@ -21,6 +36,7 @@ export class FormComponent {
     this.state.changes.subscribe((upd) => this.handleStateChange(upd));
     this.messageService.reportMessage(new Message('Creating New Product'));
   }
+
   handleStateChange(newState: StateUpdate) {
     this.editing = newState.mode == MODES.EDIT;
     if (this.editing && newState.id) {
@@ -31,16 +47,29 @@ export class FormComponent {
       this.messageService.reportMessage(
         new Message(`Editing ${this.product.name}`)
       );
+      // this.nameField.setValue(this.product.name);
+
+      // this.categoryField.setValue(this.product.category);
     } else {
       this.product = new Product();
       this.messageService.reportMessage(new Message('Creating New Product'));
+      // this.nameField.setValue('');
+      // this.categoryField.setValue('');
     }
+    this.productForm.reset(this.product);
   }
-  submitForm(form: NgForm) {
-    if (form.valid) {
+
+  submitForm() {
+    if (this.productForm.valid) {
+      Object.assign(this.product, this.productForm.value);
       this.model.saveProduct(this.product);
       this.product = new Product();
-      form.resetForm();
+      this.productForm.reset();
     }
+  }
+  resetForm() {
+    this.editing = true;
+    this.product = new Product();
+    this.productForm.reset();
   }
 }
