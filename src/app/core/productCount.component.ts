02713a122e4dedf19ced6853864645d7289c7c7d
jaime rey <jaimereycasado@gmail.com>
2023-06-13 19:46:50 +0200

    Creating Child Routes

--- src/app/core/productCount.component.ts
+++ src/app/core/productCount.component.ts
@@ -5,6 +5,8 @@ import {
   ChangeDetectorRef,
 } from '@angular/core';
 import { Model } from '../model/repository.model';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'paProductCount',
   template: `<div class="bg-info text-white p-2">
@@ -14,11 +16,23 @@ import { Model } from '../model/repository.model';
 export class ProductCountComponent {
   private differ?: KeyValueDiffer<any, any>;
   count: number = 0;
+
+  private category?: string;
   constructor(
     private model: Model,
     private keyValueDiffers: KeyValueDiffers,
-    private changeDetector: ChangeDetectorRef
-  ) {}
+    private changeDetector: ChangeDetectorRef,
+    activeRoute: ActivatedRoute
+  ) {
+    activeRoute.pathFromRoot.forEach((route) =>
+      route.params.subscribe((params) => {
+        if (params['category'] != null) {
+          this.category = params['category'];
+          this.updateCount();
+        }
+      })
+    );
+  }
   ngOnInit() {
     this.differ = this.keyValueDiffers.find(this.model.getProducts()).create();
   }
@@ -28,6 +42,10 @@ export class ProductCountComponent {
     }
   }
   private updateCount() {
-    this.count = this.model.getProducts().length;
+    this.count = this.model
+      .getProducts()
+      .filter(
+        (p) => this.category == null || p.category == this.category
+      ).length;
   }
 }
