30942e2289ff46ef7d8fd3eb2d72488d52485f9c
jaime rey <jaimereycasado@gmail.com>
2023-06-18 20:23:04 +0200

    testing with karma

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,13 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { TableComponent } from './core/table.component';
 import { FormComponent } from './core/form.component';
 import { NotFoundComponent } from './core/notFound.component';
-import { UnsavedGuard } from './core/unsaved.guard';
 const routes: Routes = [
   {
-    path: 'form/:mode/:id',
-    component: FormComponent,
-    canDeactivate: [UnsavedGuard],
+    path: "ondemand",
+    loadChildren: () => import("./ondemand/ondemand.module")
+      .then(m => m.OndemandModule)
   },
+  { path: "", redirectTo: "/ondemand", pathMatch: "full" },
   { path: 'form/:mode', component: FormComponent },
   { path: 'table', component: TableComponent },
   { path: 'table/:category', component: TableComponent },
--- src/app/ondemand/first.component.ts
+++ src/app/ondemand/first.component.ts
@@ -1,6 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Product } from "../model/product.model";
+import { Model } from "../model/repository.model";
 @Component({
-  selector: 'first',
-  template: `<div class="bg-primary text-white p-2">First Component</div>`,
+  selector: "first",
+  template: `<div class="bg-primary p-a-1">
+There are
+<span class="strong"> {{getProducts().length}} </span>
+products
+</div>`
 })
-export class FirstComponent {}
+export class FirstComponent {
+  constructor(private repository: Model) { }
+  category: string = "Soccer";
+  getProducts(): Product[] {
+    return this.repository.getProducts()
+      .filter(p => p.category == this.category);
+  }
+}
